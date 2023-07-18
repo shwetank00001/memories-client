1b0762c44ad59774163ce519734c6cc26d9f2fef
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-07-18 15:50:17 +0530

    added new work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React from 'react'
+import {Container , AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import memories from './images/memories.jpg'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+
+// typography this typo is for assigning the tags 
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h3' align='center'> Social Activity</Typography>  
+        <img src={memories} alt='memories' height="100"/>
+      </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing="3">
+            
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+
+          </Grid>
+
+        </Container>
+      </Grow>
+
+    </Container>
   );
 }
 
