b7bb93704cda01262af7feb95638790dcba5ba5a
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-07-22 21:41:24 +0530

    updated redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import {createStore,applyMiddleware, compose} from 'react-redux'
+import { Provider } from 'react-redux';
+import { thunk } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
