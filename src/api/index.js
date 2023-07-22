ac8d77ce7a7a213a0a720bb1bb6b7b4b0103afbf
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-07-23 01:17:27 +0530

    fixed error

--- src/api/index.js
+++ src/api/index.js
@@ -3,4 +3,14 @@ import axios from 'axios'
 const url = 'http://localhost:5000/posts'
 
 export const fetchPosts = () => axios.get(url)
-export const createPost = ( newPost ) => axios.post(url, newPost)
+
+export const createPost = (newPost) => (dispatch) => {
+  return axios.post(url, newPost)
+    .then((response) => {
+      dispatch({ type: 'CREATE', payload: response.data });
+    })
+    .catch((error) => {
+      console.error('Error creating post:', error);
+    })
+
+  }
