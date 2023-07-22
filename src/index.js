82e6efc5326e7b249e7e8d253624defb8783af2e
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-07-22 21:46:28 +0530

    fixing thunk

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import {createStore,applyMiddleware, compose} from 'react-redux'
+import {createStore,applyMiddleware, compose} from 'redux'
 import { Provider } from 'react-redux';
-import { thunk } from 'react-redux'
+import { thunk } from 'redux-thunk'
+
+import reducers from './reducers'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
